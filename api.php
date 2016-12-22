152e8daf8f623c0604ed9e8f8cbc20c1fc80a5dd
Alihuseyn Gulmammadov <alihuseyn13@gmail.com>
2016-12-22 13:41:47 +0300

    Style change on api and src/Kahyam/API

--- api.php
+++ api.php
@@ -1,22 +1,24 @@
 <?php
 
 
-require "src/autoload.php";
+require 'src/autoload.php';
 
-$username = ""; # Username For Kahyam Commercial
-$api_key  = ""; # Api Key For Kahyam Commercial
+$username = ''; # Username For Kahyam Commercial
+$api_key  = ''; # Api Key For Kahyam Commercial
 
 $request = new \Kahyam\Request();
 $request->setMethod(Kahyam\Request::PUT)
         ->setEndPoint(Kahyam\Endpoint::SALES)
-        ->setBody('phone','5077352617')
+        ->setBody('phone', '5077352617')
         ->setID('KHY0001638');
 
-$api = new \Kahyam\API($username,$api_key);
+$api = new \Kahyam\API($username, $api_key);
 $response = $api->setRequest($request)->getResponse();
 
-if(!$response->isError()){
+if(!$response->isError()) {
     print_r($response->output());
-}else{
+
+} else {
     print_r($response->error());
+
 }
--- src/Kahyam/API.php
+++ src/Kahyam/API.php
@@ -3,61 +3,72 @@
 namespace Kahyam;
 
 
-class API{
+class API
+{
 
     private $connection = null;
-    private $request    = null;
-    private $response   = null;
+    private $request = null;
+    private $response = null;
 
-    private $username   = null;
-    private $api_key    = null;
+    private $username = null;
+    private $api_key = null;
 
-    public function __construct($username=null,$api_key=null){
-        $this->connection = new Connection($username,$api_key);
+    public function __construct($username=null,$api_key=null)
+    {
+        $this->connection = new Connection($username, $api_key);
     }
 
     /**
      * @param $username
      */
-    public function setUsername($username){
+    public function setUsername($username)
+    {
         $this->username = $username;
     }
 
     /**
      * @param $api_key
      */
-    public function setApiKey($api_key){
+    public function setApiKey($api_key)
+    {
         $this->api_key = $api_key;
     }
 
-    public function getUsername(){
+    public function getUsername()
+    {
         return $this->username;
     }
 
-    public function getApiKey(){
+    public function getApiKey()
+    {
         return $this->api_key;
     }
 
-    public function setRequest(Request $request){
+    public function setRequest(Request $request)
+    {
         $this->request = $request;
         $this->connection->setRequest($this->request);
         $this->curl();
         return $this;
     }
 
-    public function getRequest(){
+    public function getRequest()
+    {
         return $this->request;
     }
 
-    public function getResponse(){
+    public function getResponse()
+    {
         return $this->response;
     }
 
-    protected function curl(){
+    protected function curl()
+    {
         $output = $this->connection->connect();
         $this->response = new Response($output);
         return $this;
     }
+
 
 
 }
